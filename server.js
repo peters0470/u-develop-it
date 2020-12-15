2586246ad025d9e76dcde502b4a58674e9e64782
unknown <peters0470@gmail.com>
2020-12-15 15:58:02 -0600

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -58,6 +59,30 @@ app.get('/api/candidates', (req, res) => {
     });
   });
   
+  // Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
+// ES5 function, not arrow function, to use `this`
+db.run(sql, params, function(err, result) {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+
+  res.json({
+    message: 'success',
+    data: body,
+    id: this.lastID
+  });
+});
+  });
 
   app.use((req, res) => {
     res.status(404).end();
@@ -71,17 +96,6 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
   });
 
- // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
 
 db.on('open', () => {
 app.listen(PORT, () => {
